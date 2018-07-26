1fe686aa02f1c072626575b2df7fdbabf6fd41aa
Andy Liang <andy.liang.1@stonybrook.edu>
2018-07-26 16:30:18 -0700

    Added React

---
+++ prestige/front-end/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    fetch('/results').then((res) => {
+      return res.json()
+    }).then((json) => {
+      console.log(json)
+    })
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
