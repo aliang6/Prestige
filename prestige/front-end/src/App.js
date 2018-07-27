321b671c86c98c8d38a9c0a7d902bff4c2525623
Andy Liang <andy.liang.1@stonybrook.edu>
2018-07-26 17:31:11 -0700

    Results Component

--- prestige/front-end/src/App.js
+++ prestige/front-end/src/App.js
@@ -4,11 +4,17 @@ import './App.css';
 
 class App extends Component {
   render() {
+    var report;
+    var article_title;
     fetch('/results').then((res) => {
-      return res.json()
+      return res.json();
     }).then((json) => {
-      console.log(json)
-    })
+      console.log(json);
+      report = json;
+      article_title = report.article.title;
+    });
+    console.log(article_title);
+    console.log(report);
 
     return (
       <div className="App">
@@ -19,6 +25,27 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {article_title}
+      </div>
+    );
+  }
+}
+
+class Results extends Component {
+  render() {
+    var report;
+    fetch('/results').then((res) => {
+      return res.json();
+    }).then((json) => {
+      console.log(json);
+      report = json;
+    })
+
+    const article_title = (<h1>{report.article.title}</h1>);
+
+    return (
+      <div>
+        {article_title}
       </div>
     );
   }
--- prestige/front-end/src/App.test.js
+++ prestige/front-end/src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Results from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Results />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
